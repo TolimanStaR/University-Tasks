0e27022fa1212474067875d8d03e839aee652d12
TolimanStaR <toliman.st4r@gmail.com>
2020-06-04 03:04:44 +0300

    Created project chat

---
+++ ChatClient/cmake-build-debug/CMakeFiles/ChatClient.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChatClient.dir/main.c.obj"
+  "ChatClient.exe"
+  "ChatClient.exe.manifest"
+  "ChatClient.pdb"
+  "libChatClient.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ChatClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ChatServer/cmake-build-debug/CMakeFiles/ChatServer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChatServer.dir/main.c.obj"
+  "ChatServer.exe"
+  "ChatServer.exe.manifest"
+  "ChatServer.pdb"
+  "libChatServer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ChatServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
